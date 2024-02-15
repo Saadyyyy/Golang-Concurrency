815ecaf5ec67429b4d10921fd779c2c5604253ef
Saadyyyy <laodesaady12345@gmail.com>
2024-02-15 17:18:03 +0700

    add Unit testing for car details

---
+++ service/car_detail_service_test.go
@@ -0,0 +1,19 @@
+package service
+
+// import (
+// 	"testing"
+
+// 	"github.com/stretchr/testify/assert"
+// )
+
+// func TestGetDetails(t *testing.T) {
+// 	carDetailsService := NewCarDetailService()
+// 	carService := carDetailsService.GetDetail()
+// 	assert.NotNil(t, carService)
+
+// 	assert.Equal(t, 1, carService.ID)
+// 	assert.Equal(t, "Mitsubishi", carService.Brand)
+// 	assert.Equal(t, "Montero", carService.Model)
+// 	assert.Equal(t, 2002, carService.Year)
+
+// }
