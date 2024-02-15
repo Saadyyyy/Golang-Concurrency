ab68a10660cc1b39f67fc6425832baf29caee365
Saadyyyy <laodesaady12345@gmail.com>
2024-02-15 17:15:16 +0700

    Add main

--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"Golang-Concurrency/handler"
+	"Golang-Concurrency/routes"
+	"Golang-Concurrency/service"
 )
 
-// Ping-Pong sederhana
-// buat sebuah aplikasi sederhana dengan kriteria
-// 2 pemain ping-pong saling membalikan bola
-// pemainan berhenti setelah 1 detik
-// gunakan konkurensi dengan 1 channel
+var (
+	carDetailsService    service.CarDetailService     = service.NewCarDetailService()
+	CarDetailsController handler.CarDetailsController = handler.NewCarDetailsController(carDetailsService)
+	httpRouter           routes.Router                = routes.NewChRouter()
+)
+
 func main() {
-	table := make(chan *Ball)
-
-	go Player("laode", table)
-	go Player("Saady", table)
-
-	table <- new(Ball)
-	time.Sleep(1 * time.Second)
-	<-table
-}
-
-type Ball struct {
-	hits int
-}
-
-func Player(name string, table chan *Ball) {
-	for {
-		ball := <-table
-		ball.hits++
-		fmt.Println(name, "Hits The Ball", ball.hits)
-		time.Sleep(50 * time.Millisecond)
-		table <- ball
-	}
+	const port string = ":8080"
+	httpRouter.GET("/carDestails", CarDetailsController.GetCarDetails)
+	httpRouter.SERVE(port)
 }
